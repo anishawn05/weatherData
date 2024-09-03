2167839ab24a4e00f672e332a964a1d7b7b2dd09
anishawn05 <anishawn05@gmail.com>
2024-09-03 18:40:05 +0530

    second code

--- src/main/java/com/weather/dao/WeatherAlertRepository.java
+++ src/main/java/com/weather/dao/WeatherAlertRepository.java
@@ -3,6 +3,8 @@ package com.weather.dao;
 import com.weather.model.WeatherAlert;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WeatherAlertRepository extends JpaRepository<WeatherAlert,Long> {
 }
--- src/main/java/com/weather/dao/WeatherDataRepository.java
+++ src/main/java/com/weather/dao/WeatherDataRepository.java
@@ -2,6 +2,8 @@ package com.weather.dao;
 
 import com.weather.model.WeatherData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WeatherDataRepository extends JpaRepository<WeatherData , Long> {
 }
